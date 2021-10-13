0ace69b5c58ae6993f4421fa50bbfb1aa1b07b3d
tranchau989 <ct105@hw.ac.uk>
2021-10-13 19:42:24 +0200

    Generate a custom metric and callback function

--- script_tunning/.ipynb_checkpoints/Hyperband_1Datasets-checkpoint.py
+++ script_tunning/.ipynb_checkpoints/Hyperband_1Datasets-checkpoint.py
@@ -5,7 +5,7 @@ from tensorflow.keras.regularizers import l2
 from tensorflow.keras.optimizers import Adam, SGD
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
 import tensorboard
-import kerastuner as kt #(kt.tuners.RandomSearch, kt.tuners.Hyperband)
+import keras_tuner as kt #(kt.tuners.RandomSearch, kt.tuners.Hyperband)
 from kerastuner_tensorboard_logger import (
     TensorBoardLogger,
     setup_tb  # Optional
@@ -35,19 +35,67 @@ np.random.seed(RANDOM_SEED)
 tf.random.set_seed(RANDOM_SEED)
 tf.get_logger().setLevel('ERROR')
 tf.autograph.set_verbosity(1)
+
+# disable GPU and anable MKL OneDNN
 tf.config.set_visible_devices([], 'GPU')
 os.environ['TF_ENABLE_ONEDNN_OPTS'] = '1'
-os.environ['DNNL_VERBOSE'] = '1'
+os.environ['DNNL_VERBOSE'] = '0'
+
+# reduce number of threads
+os.environ['TF_NUM_INTEROP_THREADS'] = '1' 
+os.environ['TF_NUM_INTRAOP_THREADS'] = '1' 
+
 snapshot = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
 path = '../Version9.128timesteps'
 FILESNUMBER = 1
 LSTMNUMBER  = 1
-lstm_af = ['relu', 'sigmoid', 'tanh', 'softmax', 'softsign', 'selu', 'elu']
+
+lstm_af = ['tanh', 'relu', 'sigmoid', 'softmax', 'softsign', 'selu', 'elu']
 lstm_raf = ['sigmoid']
-dense_af = ['sigmoid']
-learning_rates = [1e-2, 1e-3, 1e-4]
+dense_af = ['tanh','sigmoid', 'softmax', 'softsign']
+learning_rates = [1e-3, 1e-2, 1e-4]
 thresholds = [0.5, 0.51, 0.6, 0.7]
+optimizers = ['adam', 'sgd']
 lossmethod = ['mse']
+
+def fromBit( b ) :
+    if b == 0.0 :
+        return -0.9
+    return 0.9
+def isCorrect( target, actual ) :
+    if target < 0.0 :
+        y1 = False
+    else :
+        y1 = True
+    if actual < 0.0 :
+        y2 = False
+    else :
+        y2 = True
+    return y1 == y2 
+def customMetricfn(true, pred):
+    count = 0
+    numCorrect = 0
+    for i in range( pred.shape[ 0 ] ) :
+        for j in range( pred.shape[ 1 ] ) :
+            count += 1
+            if isCorrect( true[ i, j ], pred[ i, j ] ) :
+                numCorrect += 1
+    return (numCorrect/count)
+
+class TerminateOnZero(tf.keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs=None):
+        value = logs.get('val_customMetricfn')
+        if (value is not None):
+            if value < 0.50:
+                print(f'\nEpoch {epoch}: Val acccuracy {value} is too low, terminating training')
+                self.model.stop_training = True
+    
+    def on_batch_end(self, batch, logs=None):
+        value = logs.get('customMetricfn')
+        if (value is not None):
+            if value <= 0.40 and batch > 2000:
+                print(f'\nBatch {batch}: Acccuracy {value} is too low, terminating training')
+                self.model.stop_training = True
 
 def seperateValues(data, noIn, noOut):
     x_data, y_data = None, None
@@ -61,7 +109,7 @@ def seperateValues(data, noIn, noOut):
 def tunner_lstm_model_v1(hp):
     """Builds a recurrent model."""
     model = tf.keras.Sequential()
-    model.add(tf.keras.layers.Input(shape=(127, 3)))
+    model.add(tf.keras.layers.Input(shape=(128, 3)))
     model.add(tf.keras.layers.LSTM(units=8, 
                    activation=hp.Choice('af_LSTM', lstm_af),
                    recurrent_activation=hp.Choice('raf_LSTM', lstm_raf),
@@ -71,16 +119,13 @@ def tunner_lstm_model_v1(hp):
                    return_sequences=False))
 
     model.add(tf.keras.layers.Dense(5, hp.Choice('af_dense', dense_af)))
-    if (hp.Choice('optimizer', ['adam', 'sgd']) == 'adam'):
+    if (hp.Choice('optimizer', optimizers) == 'adam'):
         optimizer = tf.keras.optimizers.Adam(hp.Choice('learning_rate', values=learning_rates))
     else:
         optimizer = tf.keras.optimizers.SGD(hp.Choice('learning_rate', values=learning_rates))
-    model.compile(optimizer=optimizer,
-                  loss=hp.Choice('lm_LSTM', lossmethod),
-                  metrics=[tf.keras.metrics.BinaryAccuracy(threshold=hp.Choice('thresholds_BA', thresholds)),
-#                            tf.keras.metrics.Precision(name='precision'),
-#                            tf.keras.metrics.Recall(name='recall'),
-                          ])
+
+    model.compile(optimizer=optimizer, loss = 'mse', metrics=['mse', customMetricfn])
+
     return model
 
 def main(datasetpath, lossmethod):
@@ -93,7 +138,7 @@ def main(datasetpath, lossmethod):
     timestep = datasetpath.split('.')[7].split('s')[1]
     version = datasetpath.split('.')[8].split('n')[1]
     dataset  = datasetpath.split('.')[-2]
-    filename = "{}_{}_{}_{}_{}_{}".format(ni, no, mc, timestep, version, dataset)
+    filename = "{}_{}_{}_{}_{}_{}_{}".format(lossmethod[0], ni, no, mc, timestep, version, dataset)
 
     print('Dataset: ', filename)
 
@@ -109,6 +154,23 @@ def main(datasetpath, lossmethod):
     print('Step 2: Separating values and labels.')
     x_train, y_train = seperateValues(df_train, noInput, noOutput)
     x_val, y_val = seperateValues(df_val, noInput, noOutput)
+    for i in range( x_train.shape[ 0 ] ) :
+        for j in range( x_train.shape[ 1 ] ) :
+            for k in range( x_train.shape[ 2 ] ) :
+                x_train[ i, j, k ] = fromBit( x_train[ i, j, k ] )
+
+    for i in range( y_train.shape[ 0 ] ) :
+        for j in range( y_train.shape[ 1 ] ) :
+            y_train[ i, j ] = fromBit( y_train[ i, j ] )
+
+    for i in range( x_val.shape[ 0 ] ) :
+        for j in range( x_val.shape[ 1 ] ) :
+            for k in range( x_val.shape[ 2 ] ) :
+                x_val[ i, j, k ] = fromBit( x_val[ i, j, k ] )
+
+    for i in range( y_val.shape[ 0 ] ) :
+        for j in range( y_val.shape[ 1 ] ) :
+            y_val[ i, j ] = fromBit( y_val[ i, j ] )
     print("+ Training set:   ", x_train.shape, y_train.shape, x_train.dtype)
     print("+ Validating set: ", x_val.shape, y_val.shape, x_val.dtype)
     
@@ -117,16 +179,16 @@ def main(datasetpath, lossmethod):
     log_dir_tuner   = "../logs//tuner//"   + filename + "//" + lossmethod[0] + "//"
     lstm_tuner_v1=kt.tuners.Hyperband(
         tunner_lstm_model_v1,
-        objective=kt.Objective('val_binary_accuracy', direction='max'),
+        objective=kt.Objective('val_customMetricfn', direction='max'),
         max_epochs=8,
         seed=42,
         factor=3,
-        hyperband_iterations=1,
+        hyperband_iterations=2,
         directory=log_dir_tuner,
         project_name="MasterProject",
         overwrite=True,
         logger=TensorBoardLogger(
-            metrics=['loss', 'binary_accuracy', 'val_loss', 'val_binary_accuracy'], logdir=log_dir_hparams,
+            metrics=['loss','val_loss', 'customMetricfn', 'val_customMetricfn'], logdir=log_dir_hparams,
         ) # add only this argument
     )
     
@@ -136,9 +198,9 @@ def main(datasetpath, lossmethod):
                          batch_size=1,
                          validation_data=(x_val, y_val),
                          shuffle=True,
-                         use_multiprocessing=True,
-                         workers=4,
-                         callbacks=[tf.keras.callbacks.EarlyStopping("val_binary_accuracy")]
+                         use_multiprocessing=False,
+#                          callbacks=[tf.keras.callbacks.TerminateOnNaN()]
+                         callbacks=[tf.keras.callbacks.EarlyStopping("val_customMetricfn"), tf.keras.callbacks.TerminateOnNaN(), TerminateOnZero()]
                         )
     bestparams_v1 = lstm_tuner_v1.get_best_hyperparameters(1)[0]
     hyper_model_v1 = lstm_tuner_v1.hypermodel.build(bestparams_v1)
@@ -146,34 +208,47 @@ def main(datasetpath, lossmethod):
     print('Step 4: Rerun with the best tuning configuration.')
     strt_time = datetime.datetime.now() 
     training_history_v1 = hyper_model_v1.fit(
-        x_train, # input
-        y_train, # output
+        x_train, y_train, 
         batch_size=1,
-        verbose=0, # Suppress chatty output; use Tensorboard instead
+        verbose=1, # Suppress chatty output; use Tensorboard instead
         epochs=8,
-        validation_data=(x_val, y_val))
+        validation_data=(x_val, y_val),
+        shuffle=True,
+        callbacks=[tf.keras.callbacks.EarlyStopping("val_customMetricfn"), tf.keras.callbacks.TerminateOnNaN(), TerminateOnZero()]
+    )
     curr_time = datetime.datetime.now()
     timedelta = curr_time - strt_time
     
     val_performance = hyper_model_v1.evaluate(x_val, y_val)
     
+    y_pred = hyper_model_v1.predict(x_val, verbose=1)
+    count = 0
+    numCorrect = 0
+    for i in range( y_pred.shape[ 0 ] ) :
+        for j in range( y_pred.shape[ 1 ] ) :
+            count += 1
+            if isCorrect( y_val[ i, j ], y_pred[ i, j ] ) :
+                numCorrect += 1
+    
     print('Step 5: Saving result.')
     tuning_result = {**{'project': lstm_tuner_v1.project_name},
                      **{'log_dir': lstm_tuner_v1.project_dir},
                      **{'dataset_no': filename},
+                     **{'dataset_name': datasetpath},
                      **{'objectives': '{}, {}'.format(lstm_tuner_v1.oracle.objective.name, lstm_tuner_v1.oracle.objective.direction)},
                      **(lstm_tuner_v1.oracle.get_best_trials(1)[0].hyperparameters.values), 
                      **{'tuned_score': round(lstm_tuner_v1.oracle.get_best_trials(1)[0].score, 5)},
                      **{'loss': round(val_performance[0],5)},
-                     **{'binary_accuracy': round(val_performance[1],5)},
-#                      **{'precision': round(val_performance[2],5)},
-#                      **{'recall': round(val_performance[3],5)},
+                     **{'val_loss': round(val_performance[1],5)},
+                     **{'customMetricfn': round(val_performance[2],5)},
+                     **{'val_customMetricfn': round(val_performance[3],5)},
+                     **{'pred_accuracy': round(customMetricfn(y_val, y_pred), 5)},
                      **{'training_time (ms)': round(timedelta.total_seconds(),5)},
                     }
     [tuning_result.pop(key, None) for key in ['tuner/initial_epoch', 'tuner/bracket', 'tuner/round']]
     
     df = (pd.DataFrame.from_dict(tuning_result, orient='index', columns=[str(tuning_result['dataset_no'])]))
-    df.to_csv('../results/{}_{}.csv'.format(filename,lossmethod[0]), index=True, index_label='Items')
+    df.to_csv('../results/{}.csv'.format(filename), index=True, index_label='Items')
 
 if __name__=="__main__":
     print("No. of arguments passed is ", len(sys.argv))
@@ -185,5 +260,7 @@ if __name__=="__main__":
         datasetpath = sys.argv[2]
     else:
         print("Don't have sufficient arguments.")
+    
     main(datasetpath, lossmethod)
+    
     print("Complete ", datasetpath)
--- script_tunning/tasksdividing.sh
+++ script_tunning/tasksdividing.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-readonly CORE_IDLE_THRESHOLD=95;
+readonly CORE_IDLE_THRESHOLD=75;
 readonly CORE_USED_THRESHOLD=25;
-readonly TOP_LOOP_NUMBER=5;
+readonly TOP_LOOP_NUMBER=3;
 readonly TOP_DELAY_NUMBER=2;
 readonly SLEEP_COUNT=0.1; #in second
 files_array=();
@@ -10,7 +10,7 @@ cpus_array=(8 9 10 11 12 13 14 15);
 cpus_array_length=${#cpus_array[@]};
 cpus_assigned_array=();
 cpu_index=0;
-lossmethod="binary_crossentropy"
+lossmethod="mse"
 
 for file in ../Version9.128timesteps/*; do
     files_array+=($file);
@@ -24,21 +24,19 @@ starttime_date=`date`;
 starttime_second=`date +%s`;
 echo "Starting Hyperparameter Tunning with ${#files_array[@]} total file on $starttime_date ($starttime_second).";
 
-for file in "${files_array[@]}"; 
-do
+for file in "${files_array[@]}"; do
     ni_no=$(echo $file | cut -d '/' -f 3 | cut -d '.' -f 2 | cut -d '=' -f 2);
     no_no=$(echo $file | cut -d '/' -f 3 | cut -d '.' -f 3 | cut -d '=' -f 2);
     mc_no=$(echo $file | cut -d '/' -f 3 | cut -d '.' -f 4 | cut -d '=' -f 2);
     timestep_no=$(echo $file | cut -d '/' -f 3 | cut -d '.' -f 5 | cut -d 's' -f 2);
     version_no=$(echo $file | cut -d '/' -f 3 | cut -d '.' -f 6 | cut -d 'n' -f 2);
     file_no=$(echo $file | cut -d '/' -f 3 | cut -d '.' -f 7);
-    filename="$ni_no""_""$no_no""_""$mc_no""_""$timestep_no""_""$version_no""_""$file_no";
+    filename="$lossmethod""_""$ni_no""_""$no_no""_""$mc_no""_""$timestep_no""_""$version_no""_""$file_no";
     flag=0;
     while : 
     do
         cpu=${cpus_array[$cpu_index]};
         cpu_str="Cpu$((cpu + 1))"
-
         core_idle_values=$(top -b -n "$TOP_LOOP_NUMBER" -d "$TOP_DELAY_NUMBER" | grep "$cpu_str" | awk '{print $3}' | cut -f 1 -d '.');
         readarray -t core_idle_lines <<< "$core_idle_values"
 
@@ -53,16 +51,13 @@ do
             ((total++));
         done
         core_idle=$((100 - sum/total));
-
         if [[ $core_idle -gt $CORE_IDLE_THRESHOLD ]]; then
             log_dir="../logs/processing/log_${filename}.txt"
             if [[ "${cpus_assigned_array[$cpu_index]}" == "0" ]]; then
-                `taskset -c "$cpu"  python ./Hyperband_1Datasets.py "$lossmethod" "$file" &>"$log_dir" &!`;
                 cpus_assigned_array[$cpu_index]=$filename;
                 flag=1;
             else
-                if [[ $(tail -1 ../logs/processing/log_"${cpus_assigned_array[$cpu_index]}".txt) == *"Complete"* ]]; then
-                    `taskset -c "$cpu"  python ./Hyperband_1Datasets.py "$lossmethod" "$file" &>"$log_dir" &!`;
+                if [[ $(tail -1 ../logs/processing/log_"${cpus_assigned_array[$cpu_index]}".txt) == *"Complete"* ]] || [[ $(tail -1 ../logs/processing/log_"${cpus_assigned_array[$cpu_index]}".txt) == *"Error"* ]]; then                    
                     cpus_assigned_array[$cpu_index]=$filename;
                     flag=1;
                 fi
@@ -76,11 +71,44 @@ do
         fi
 
         if [[ "$flag" -eq "1" ]]; then
+            `taskset -c "$cpu"  python ./Hyperband_1Datasets.py "$lossmethod" "$file" &>"$log_dir" &!`;
             echo "Assigning $cpu_str to file number $filename";
             break
         fi
     done
     sleep $SLEEP_COUNT;
+done
+
+# Check if all the processes has finished
+idlecores=1;
+while : 
+do
+    cpu=${cpus_array[$cpu_index]};
+    core_idle_values=$(top -b -n "$TOP_LOOP_NUMBER" -d "$TOP_DELAY_NUMBER" | grep "$cpu_str" | awk '{print $3}' | cut -f 1 -d '.');
+    readarray -t core_idle_lines <<< "$core_idle_values"
+
+    total=0;
+    sum=0;
+    for i in "${core_idle_lines[@]}"; do
+        if [[ ! -z "$i" ]]; then
+            sum=$(($sum + $i));
+        else
+            sum=$(($sum + 0));
+        fi
+        ((total++));
+    done
+    core_idle=$((100 - sum/total));
+    if [[ $core_idle -gt $CORE_IDLE_THRESHOLD ]]; then
+        ((idlecores+=1))
+    fi
+    if (( $idlecores == $cpus_array_length)); then
+        break
+    fi
+    if [[ $cpu_index -ge $(($cpus_array_length - 1)) ]]; then
+        cpu_index=0;
+    else
+        ((cpu_index += 1));
+    fi
 done
 
 endtime_date=`date`;
