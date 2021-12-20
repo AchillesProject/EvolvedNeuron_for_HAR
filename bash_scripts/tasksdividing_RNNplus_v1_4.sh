5bba9ca356646538730463c43d3625c521bc3b43
Tran Chau <ct105@hw.ac.uk>
2021-12-20 18:36:29 +0100

    Fix core_id value

--- bash_scripts/tasksdividing_RNNplus_v1_4.sh
+++ bash_scripts/tasksdividing_RNNplus_v1_4.sh
@@ -36,7 +36,7 @@ for file in "${files_array[@]}"; do
     do
         cpu=${cpus_array[$cpu_index]};
         cpu_str="Cpu$((cpu + 1))"
-        core_idle_values=$(top -b -n "$TOP_LOOP_NUMBER" -d "$TOP_DELAY_NUMBER" | grep "$cpu_str" | awk -F: '{print $2}' | awk '{print $1}' | cut -f 1 -d '.');
+        core_idle_values=$(top -b -n "$TOP_LOOP_NUMBER" -d "$TOP_DELAY_NUMBER" | grep "Cpu$((cpu + 1))" | awk -F: '{print $2}' | awk '{print $1}' | cut -f 1 -d '.');
         readarray -t core_idle_lines <<< "$core_idle_values"
 
         total=0;
@@ -83,7 +83,7 @@ idlecores=1;
 while : 
 do
     cpu=${cpus_array[$cpu_index]};
-    core_idle_values=$(top -b -n "$TOP_LOOP_NUMBER" -d "$TOP_DELAY_NUMBER" | grep "$cpu_str" | awk -F: '{print $2}' | awk '{print $1}' | cut -f 1 -d '.');
+    core_idle_values=$(top -b -n "$TOP_LOOP_NUMBER" -d "$TOP_DELAY_NUMBER" | grep "Cpu$((cpu + 1))" | awk -F: '{print $2}' | awk '{print $1}' | cut -f 1 -d '.');
     readarray -t core_idle_lines <<< "$core_idle_values"
 
     total=0;
