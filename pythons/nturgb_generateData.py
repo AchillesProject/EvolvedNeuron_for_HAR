54c16745aba54b3f25c3eaade4e009658a95bcb2
tranchau989 <ct105@hw.ac.uk>
2022-01-06 00:16:43 +0100

    testing

--- pythons/nturgb_generateData.py
+++ pythons/nturgb_generateData.py
@@ -34,9 +34,9 @@ for seq_no in seq_no_arr:
                         frameset = np.concatenate((frameset, file[frame].reshape(file.shape[1], 1, file.shape[2])), axis=1)
             fileset = np.concatenate((fileset, np.tile(int(k.split('.')[-2].split('A')[-1]), fileset.shape[0]).reshape(-1, 1)), axis=1)
             dataset = fileset if dataset.shape[0] == 0 else np.concatenate((dataset, fileset), axis=0)
-            display(dataset.shape)
 
-    with open(f"../../Datasets/5_nturgb+d/nturgb+d.ni={3}.no={60}.ts={seq_no}.bs={50}.csv",'w') as csvfile:
-        np.savetxt(csvfile, np.array([[3, 60]]),fmt='%d', delimiter=",")
-    with open(f"../../Datasets/5_nturgb+d/nturgb+d.ni={3}.no={60}.ts={seq_no}.bs={50}.csv",'a') as csvfile:
-        np.savetxt(csvfile, testset_np, fmt='%.4f', delimiter=",")
+            with open(f"../../Datasets/5_nturgb+d/nturgb+d.ni={3}.no={60}.ts={seq_no}.bs={50}.csv",'w') as csvfile:
+                np.savetxt(csvfile, np.array([[3, 60]]),fmt='%d', delimiter=",")
+            with open(f"../../Datasets/5_nturgb+d/nturgb+d.ni={3}.no={60}.ts={seq_no}.bs={50}.csv",'a') as csvfile:
+                np.savetxt(csvfile, dataset, fmt='%.4f', delimiter=",")
+            break
