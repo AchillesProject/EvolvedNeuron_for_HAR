25c67d42f65cc92ee17639b61c3f16673e9ec7c8
tranchau989 <ct105@hw.ac.uk>
2022-01-06 11:06:36 +0100

    Minor changes

--- pythons/nturgb_generateData.py
+++ pythons/nturgb_generateData.py
@@ -8,6 +8,7 @@ frame_dict = {}
 dataset = np.array([])
 fileset = np.array([])
 seq_no_arr = [20, 30, 40, 50, 60]
+count = 0
 
 for idx, file_name in enumerate(glob.glob(os.path.join(NPY_DIR, '*.npy'))):
     file = np.load(file_name)
@@ -16,6 +17,7 @@ for idx, file_name in enumerate(glob.glob(os.path.join(NPY_DIR, '*.npy'))):
 for seq_no in seq_no_arr:
     for k, v in frame_dict.items():
         if v > seq_no:
+            print(k)
             frameset = np.array([])
             fileset = np.array([])
             file = np.load(k)
@@ -32,11 +34,11 @@ for seq_no in seq_no_arr:
                         frameset = np.array([])
                     else:
                         frameset = np.concatenate((frameset, file[frame].reshape(file.shape[1], 1, file.shape[2])), axis=1)
+            
+
             fileset = np.concatenate((fileset, np.tile(int(k.split('.')[-2].split('A')[-1]), fileset.shape[0]).reshape(-1, 1)), axis=1)
             dataset = fileset if dataset.shape[0] == 0 else np.concatenate((dataset, fileset), axis=0)
-
-            with open(f"../../Datasets/5_nturgb+d/nturgb+d.ni={3}.no={60}.ts={seq_no}.bs={50}.csv",'w') as csvfile:
-                np.savetxt(csvfile, np.array([[3, 60]]),fmt='%d', delimiter=",")
-            with open(f"../../Datasets/5_nturgb+d/nturgb+d.ni={3}.no={60}.ts={seq_no}.bs={50}.csv",'a') as csvfile:
-                np.savetxt(csvfile, dataset, fmt='%.4f', delimiter=",")
-            break
+    with open(f"../../Datasets/5_nturgb+d/nturgb+d.ni={3}.no={60}.ts={seq_no}.bs={50}.csv",'w') as csvfile:
+        np.savetxt(csvfile, np.array([[3, 60]]),fmt='%d', delimiter=",")
+    with open(f"../../Datasets/5_nturgb+d/nturgb+d.ni={3}.no={60}.ts={seq_no}.bs={50}.csv",'a') as csvfile:
+        np.savetxt(csvfile, dataset, fmt='%.4f', delimiter=",")
