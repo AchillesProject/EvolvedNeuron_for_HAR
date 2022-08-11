50c344fd7b5fc3ddc2319df03275d6568a38e492
tranchau989 <chaut@hiof.no>
2022-08-11 07:08:34 +0000

    fulla-gpu-a100

--- pythons/new_neurons/run_8sets/best21_v27_8sets.py
+++ pythons/new_neurons/run_8sets/best21_v27_8sets.py
@@ -123,7 +123,7 @@ class RNN_plus_v1_27_cell(tf.keras.layers.LSTMCell):
             self._enable_caching_device = kwargs.pop('enable_caching_device', True)
         else:
             self._enable_caching_device = kwargs.pop('enable_caching_device', False)
-        super(RNN_plus_v1_25_cell, self).__init__(units, **kwargs)
+        super(RNN_plus_v1_27_cell, self).__init__(units, **kwargs)
         self.units = units
         self.state_size = self.units
         self.output_size = self.units
